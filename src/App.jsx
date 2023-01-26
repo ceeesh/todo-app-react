1951756798153760a53df6c904ff440b8f0d9f14
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2023-01-26 23:17:41 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+function App() {
+
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1>HELLLO</h1>
+    </div>
+  )
+}
+
+export default App
